55677a90e59c768f498d3bdf252a9f4e623f741e
AlexanderBadger <sashazateev444@gmail.com>
2023-07-30 12:26:57 +0200

    commit1

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("ja eblan");
+string username = Console.ReadLine(); // string -type of data, username variable name,
+Console.WriteLine("пишов ты нахуй");
+Console.WriteLine(username);
