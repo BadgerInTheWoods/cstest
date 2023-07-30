30e9955f685e81f6064806506083fb3e2c3f5535
AlexanderBadger <sashazateev444@gmail.com>
2023-07-30 12:58:18 +0200

    sssssssssssssssssnake

---
+++ example 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите им пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="salmon")
+{
+    Console.WriteLine("Samon yeha hey i mena hi");
+}
+else 
+{
+    Console.Write("Hi not salmon");
+    Console.WriteLine(username);
+}
